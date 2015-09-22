8850b7f58242222fa4602d0db444dc180cff17ff
Justin Holdsclaw <jjholdsclaw@gmail.com>
2015-09-21 22:17:06 -0700

    Instagram configs...somehow these were missed??

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+Instagram.configure do |config|
+  config.client_id = "7b88e4fb7e5348e9bf8cdca17e2523ef"
+  config.client_secret = "d7994d9bc9474b6e8a71092f68d5de6e"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
